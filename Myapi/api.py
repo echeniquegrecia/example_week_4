bdab587730e6fc9845f2e4f30f4ac834d88d9de8
Grecia <grecia.echenique@coding-academy.fr>
2018-08-28 16:39:35 +0200

    Added Myapi and Myapp

---
+++ Myapi/api.py
@@ -0,0 +1,107 @@
+from flask import Flask, request, jsonify
+from flask_restplus import Resource, Api
+import requests
+
+app = Flask(__name__)
+api = Api(app)
+token = "EAAf78PB51bUBAMDdGLPFpiajkRsLHLOjblIiDsKLUeHQpSgFRaZAQaYEJCYiNv7iv3CgtnJYHKzsXguBJQVrdCsjYp7maP2OqKbN5ZAi37yRKKAZAKc8ixbvqZAFDZBGQKbMisYJbuEmbAmUIuYk3uDDC6jgsXagJxNbG3h7d0gZDZD"
+
+
+@api.route('/api/facebook/all/posts')
+class FacebookPosts(Resource):
+    def get(self):
+        api_url = "https://graph.facebook.com/me?fields=posts"
+        req = requests.get(api_url, headers={'authorization': "Bearer " + token})
+        res = req.json()
+        return res['posts']
+
+@api.route('/api/facebook/all/photos')
+class FacebookPhotos(Resource):
+    def get(self):
+        api_url = "https://graph.facebook.com/me?fields=albums.fields(photos.fields(source)) "
+        req = requests.get(api_url, headers={'authorization': "Bearer " + token})
+        res = req.json()
+        list_photos=[]
+        for i in res['albums']['data']:
+            for j in i['photos']['data']:
+                result = {'source':j['source'], 'id':j['id']}
+                list_photos.append(result)
+        return list_photos
+
+
+@api.route('/api/facebook/do/post')
+class FacebookDoPost(Resource):
+    def post(self):
+        api_url="https://graph.facebook.com/me/feed"
+        message = request.form['message']
+        create_data = {"message":message}
+        send = requests.post(url=api_url, json=create_data, headers={'authorization': "Bearer " + token})
+        response = send.json()
+        return response
+
+
+@api.route('/api/facebook/all/conversations')
+class FacebookConversations(Resource):
+    def get(self):
+        api_url= "https://graph.facebook.com/me/conversations?fields=senders"
+        req = requests.get(api_url, headers={'authorization': "Bearer " + token})
+        res = req.json()
+        list_conversation = []
+        for i in res['data']:
+            data= {'id_conversation': i['id'], 'participant': i['senders']['data'][0]['name']}
+            list_conversation.append(data)
+        return list_conversation
+
+@api.route('/api/facebook/show/conversation/<id>')
+class FacebookConversationId(Resource):
+    def get(self, id):
+        api_url= "https://graph.facebook.com/" +str(id)+ "?fields=messages{message,created_time,from}"
+        req = requests.get(api_url, headers={'authorization': "Bearer " + token})
+        res = req.json()
+        list_message = []
+        for i in res['messages']['data']:
+            data = {'message': i['message'], 'time': i['created_time'], 'from': i['from']['name']}
+            list_message.append(data)
+        return list_message
+
+@api.route('/api/facebook/do/message/<id>')
+class FacebookDoMessage(Resource):
+    def post(self, id):
+        api_url="https://graph.facebook.com/" + str(id)+ "/messages"
+        message = request.form['message']
+        create_data = {"message":message}
+        send = requests.post(url=api_url, json=create_data, headers={'authorization': "Bearer " + token})
+        response = send.json()
+        return response
+
+
+@api.route('/api/facebook/load/photo')
+class FacebookLoadPhoto(Resource):
+    def post(self):
+        api_url="https://graph.facebook.com/234670333835708/photos"
+        url = request.form['url']
+        create_data = {'url':url, 'published':'true'}
+        send = requests.post(url=api_url, json=create_data, headers={'authorization': "Bearer " + token})
+        response = send.json()
+        return response
+
+@api.route('/api/facebook/delete/post/<id>')
+class FacebookDeletePhoto(Resource):
+    def delete(self, id):
+        api_url="https://graph.facebook.com/"+str(id)
+        send = requests.delete(url=api_url, headers={'authorization': "Bearer " + token})
+        response = send.json()
+        return response
+
+
+@api.route('/api/facebook/delete/photo/<id>')
+class FacebookDeletePhoto(Resource):
+    def delete(self, id):
+        api_url="https://graph.facebook.com/"+str(id)
+        send = requests.delete(url=api_url, headers={'authorization': "Bearer " + token})
+        response = send.json()
+        return response
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ Myapp/app.py
@@ -0,0 +1,92 @@
+from flask import Flask, request, render_template, jsonify, redirect
+import requests
+app = Flask(__name__, template_folder= 'views')
+
+
+token= 'EAAd6sZBomBwwBAK0e2Gue9UTQhnAiokCUEg8F15VQYT4KnIFBHlYYnfyZA3aKr5IBIc5Deg5ys4TkAspgF86xP4KJuC0f50VcZBchu7wsAR84Xew8jd1Lz20ZAA5goZBFvL9Cgu1ZCQn4YpEDl69PBKivGADAYOfAZD'
+
+@app.route("/app/facebook/home")
+def home():
+    return render_template('home.html')
+
+@app.route("/app/facebook/all/posts" , methods=('GET', 'POST'))
+def get_posts():
+    if request.method == 'GET':    
+        req= requests.get('http://127.0.0.1:5000/api/facebook/all/posts')
+        res = req.json()
+        return render_template('posts.html', posts=res)
+    else:
+        message = request.form['message']
+        if message:
+            api_url="http://127.0.0.1:5000/api/facebook/do/post"
+            create_data = {'message': message}
+            req1 = requests.post(url=api_url, data=create_data)
+            res = req1.json()
+            return redirect("/app/facebook/all/posts")
+        else:
+            return redirect("/app/facebook/all/posts")
+    return render_template('posts.html')
+
+
+@app.route("/app/facebook/all/photos", methods=('GET', 'POST'))
+def get_photos():
+    if request.method == 'GET': 
+        req= requests.get('http://127.0.0.1:5000/api/facebook/all/photos')
+        res = req.json()
+        return render_template('photos.html', photos=res)
+    else:
+        url = request.form['url']
+        published = 'true'
+        if url and published:
+            api_url="http://127.0.0.1:5000/api/facebook/load/photo"
+            create_data = {'url':url, 'published':published}
+            req1 = requests.post(url=api_url, data=create_data)
+            res = req1.json()
+            return redirect("/app/facebook/all/photos")
+        else:
+            return redirect("/app/facebook/all/photos")
+    return render_template('photos.html')
+
+
+
+@app.route("/app/facebook/all/conversations")
+def get_conversations():
+    if request.method == 'GET':    
+        req= requests.get('http://127.0.0.1:5000/api/facebook/all/conversations')
+        res = req.json()
+        return render_template('conversations.html', conversations=res)
+
+
+@app.route("/app/facebook/show/conversation/<id>" , methods=('GET', 'POST'))
+def get_conversation_id(id):
+    if request.method == 'GET':    
+        api_url = "http://127.0.0.1:5000/api/facebook/show/conversation/"+ str(id)
+        req= requests.get(api_url)
+        res = req.json()
+        return render_template('conversation_id.html', conversation=res)
+    else:
+        message = request.form['message']
+        if message:
+            api_url="http://127.0.0.1:5000/api/facebook/do/message/" + str(id)
+            create_data = {'message': message}
+            req1 = requests.post(url=api_url, data=create_data)
+            res = req1.json()
+            return redirect("/app/facebook/show/conversation/"+str(id))
+        else:
+            return redirect("/app/facebook/show/conversation/"+str(id))
+    return render_template('conversation_id.html')
+
+
+@app.route('/app/facebook/delete/post/<id>')
+def delete_post(id):
+    api_url= "http://127.0.0.1:5000/api/facebook/delete/post/"+ str(id)
+    send = requests.delete(url=api_url, headers={'authorization': "Bearer " + token})
+    response = send.json()
+    return redirect("/app/facebook/all/posts")
+
+@app.route('/app/facebook/delete/photo/<id>')
+def delete_photo(id):
+    api_url= "http://127.0.0.1:5000/api/facebook/delete/photo/"+ str(id)
+    send = requests.delete(url=api_url, headers={'authorization': "Bearer " + token})
+    response = send.json()
+    return redirect("/app/facebook/all/photos")
